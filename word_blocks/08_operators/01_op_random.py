535546e669eb8f19781641a70ef97ddeba02eea4
Azzie <azzie@astronet.pl>
2022-09-28 01:59:27 +0200

    Three interesting operators

---
+++ word_blocks/08_operators/01_op_random.py
@@ -0,0 +1,4 @@
+import urandom
+
+random = urandom.randint(1, 10) # 1 <= random <= 10
+return random
---
+++ word_blocks/08_operators/12_op_between.py
@@ -0,0 +1,2 @@
+# In fact this trivial example would be optimized out to True :-)
+return -10 <= 5 <= 10
